cc4e499d095ad372f0d712deceac52d31a219377
M. Fathony Ramdhan <42707519+mfathonyramdhan@users.noreply.github.com>
2023-09-01 10:35:56 +0700

    fix bug

--- dashboard/guru_update_process.php
+++ dashboard/guru_update_process.php
@@ -6,7 +6,6 @@ if (isset($_POST['update_teacher'])) {
     $name = $_POST['name'];
     $gender = $_POST['gender'];
     $jabatan = $_POST['jabatan'];
-    // $nip = $_POST['nip'];
     $email = $_POST['email'];
     $idClass = $_POST['class'];
     $password = $_POST['password'];
@@ -16,19 +15,21 @@ if (isset($_POST['update_teacher'])) {
         // Hash the password
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-
-        $query = "UPDATE guru SET nama='$name', gender='$gender', jabatan='$jabatan', email='$email', id_kelas='$idClass', password='$hashedPassword', WHERE id_guru='$teacherId'";
+        // Update the teacher record with the new password
+        $query = "UPDATE guru SET nama='$name', gender='$gender', jabatan='$jabatan', email='$email', id_kelas='$idClass', password='$hashedPassword' WHERE id_guru='$teacherId'";
     } else {
         // Update the teacher record without changing the password
-
         $query = "UPDATE guru SET nama='$name', gender='$gender', jabatan='$jabatan', email='$email', id_kelas='$idClass' WHERE id_guru='$teacherId'";
     }
 
-    $query = "UPDATE guru SET nama='$name', gender='$gender', jabatan='$jabatan', id_kelas='$idClass' WHERE id_guru='$teacherId'";
-
+    // Execute the update query
     $result = mysqli_query($connection, $query);
-    mysqli_close($connection);
-    header("Location: guru.php");
+
+    if ($result) {
+        header("Location: guru.php");
+    } else {
+        echo "Error updating record: " . mysqli_error($connection);
+    }
 }
 
 // Close the database connection
